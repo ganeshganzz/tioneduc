36fd5cd74f27df995bf7581697a600c3f654704d
Your Name <you@example.com>
2018-01-21 22:56:13 +0530

    Changed the HTML for the site.

--- tioneduc/views.py
+++ tioneduc/views.py
@@ -1,6 +1,4 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.shortcuts import render
+def Post_list(request):
+    return render(request, 'tioneduc/Post_list.html', {})
 
-# Create your views here.
