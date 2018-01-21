686653b193403e2dd08868d92e8ab6f812173edf
Your Name <you@example.com>
2018-01-21 23:20:28 +0530

    Modified templates to display posts from database.

--- tioneduc/views.py
+++ tioneduc/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
-def Post_list(request):
-    return render(request, 'tioneduc/Post_list.html', {})
+from django.utils import timezone
+from .models import Post
 
+def Post_list(request):
+    Posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'tioneduc/Post_list.html', {'Posts': Posts})
